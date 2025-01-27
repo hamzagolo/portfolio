6c7ef5013b03c229fb73b8f510636e69dfffec8f
GoloHamza <156569508+GoloHamza@users.noreply.github.com>
2025-01-26 18:53:36 -0800

    Create script.js
    Hey! I added another file called script.js for separation of concern

---
+++ script.js
@@ -0,0 +1,40 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxVXyyYhoEHFeiK0DuAz4da5FSBzScNwN9JuQsiNlYA0SnAKpE7w-gbj1CXOURpAGi4/exec'
+  const form = document.forms['submit-to-google-sheet']
+  constmsg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+         msg.innerHTML = "Message sent succesefully"
+         setTimeout(function(){
+          msg.innerHTML = ""
+         },5000)
+         form.reset()
+
+      })
+
+  
+      .catch(error => console.error('Error!', error.message))
+  })
+
+  // tablinks //
+  var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  Event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
